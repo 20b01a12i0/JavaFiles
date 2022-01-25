504cbea51816953dad7152d85c2689651bbdaa70
20b01a12i0 <92462104+20b01a12i0@users.noreply.github.com>
2022-01-25 19:58:18 +0530

    Add files via upload

---
+++ Point2D.java
@@ -0,0 +1,22 @@
+
+ class Point2D {
+      private int x,y;
+      
+
+	public int getX() { //getter method
+		return x;
+	}
+
+	public void setX(int x) { //setter method
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+      
+}
